6504366ca522120248df5e81f3d585817c2b7f27
pedro <Pecarvajal246@gmail.com>
2022-06-30 22:54:17 -0400

    feat: add user input validation

--- messages.js
+++ messages.js
@@ -17,8 +17,20 @@ const {
 
 // Request a la api para obtener 1 producto por su id
 async function searchProduct(msg) {
+  // Regex para validar el input del ususario, solo permite mensaje que contengan un número del 1 al 20
+  const regex = /^1?\d$|^20$/;
+  const userInput = msg.text;
   let id = msg.from.id;
-  const replyMarkup = productResultMenu;
+  let replyMarkup = productResultMenu;
+  if (!regex.test(userInput)) {
+    replyMarkup = productsMenu;
+    bot.sendMessage(
+      msg.from.id,
+      "⚠️Error, debe introducir solamente el numero del producto que desea buscar. Ej: 1.\nSeleccione una opción",
+      { replyMarkup }
+    );
+    return;
+  }
   bot.sendMessage(id, "buscando...");
   try {
     const text = await apiSearchProduct(msg);
@@ -58,7 +70,19 @@ async function postUser(msg) {
 
 // Request a la api para añadir los item al carrito del ususario
 async function addToCart(msg) {
-  const replyMarkup = cartMenu;
+  // Regex para validar el input del ususario, solo permite mensajes que contengan números del 1 al 20 separados por comas
+  const regex = /^(1?\d|^20$)+(,(1?\d|20$))*$/;
+  const userInput = msg.text;
+  let replyMarkup = cartMenu;
+  if (!regex.test(userInput)) {
+    replyMarkup = productsMenu;
+    bot.sendMessage(
+      msg.from.id,
+      "⚠️Error, debe introducir solamente los numeros de los productos separados por comas. Ej: 1,2,3.\nSeleccione una opción",
+      { replyMarkup }
+    );
+    return;
+  }
   try {
     // agrega los ids de los items al carrito del usuario
     await apiPostCart(msg);
@@ -96,46 +120,58 @@ function showMainMenu(msg) {
 }
 
 //TEXTO DELIVERY
-function textDelivery (msg) {
-  let text = '🚚Nuestros horarios y metódos de envíos:\n'
-  +'📌 Envíos al todo el país 🇻🇪\n'
-  +'📌 Trabajamos de Lunes a Sábados de 9am a 4pm⏱\n'
-  +'📌 Pedidos de zonas aledañas, serán despachadas en el día.\n'
-  +'📌 Los despachos al resto del país se realizan los días Lunes y jueves.';
-return bot.sendMessage(msg.from.id, text);
+function textDelivery(msg) {
+  let text =
+    "🚚Nuestros horarios y metódos de envíos:\n" +
+    "📌 Envíos al todo el país 🇻🇪\n" +
+    "📌 Trabajamos de Lunes a Sábados de 9am a 4pm⏱\n" +
+    "📌 Pedidos de zonas aledañas, serán despachadas en el día.\n" +
+    "📌 Los despachos al resto del país se realizan los días Lunes y jueves.";
+  return bot.sendMessage(msg.from.id, text);
 }
 
 //MENU PAYMENT
-function buttonsPayment (msg) {
+function buttonsPayment(msg) {
   const replyMarkup = payMenu;
   bot.sendMessage(msg.from.id, "⬇⚡Info sobre nuestros métodos de pago⚡⬇", {
     replyMarkup,
   });
 }
 //InfoCash
-function infoCash (msg){
+function infoCash(msg) {
   let id = msg.from.id;
-  let text = '✨ Podrá realizar su pago en efectivo cuando reciba su pedido😊(Válido para envíos en zonas cercanas)';
-  return bot.sendMessage(id,text);
+  let text =
+    "✨ Podrá realizar su pago en efectivo cuando reciba su pedido😊(Válido para envíos en zonas cercanas)";
+  return bot.sendMessage(id, text);
 }
 //InfoCrypto
-function infoCrypto (msg) {
+function infoCrypto(msg) {
   let id = msg.from.id;
-  let text = '💰 ACEPTAMOS 💰 \n'
-    + '🔥Bitcoin\n'
-    + '🔥Ethereum\n'
-    + '🔥Tether (USDT)' ;
- return bot.sendMessage(id, text);
+  let text =
+    "💰 ACEPTAMOS 💰 \n" + "🔥Bitcoin\n" + "🔥Ethereum\n" + "🔥Tether (USDT)";
+  return bot.sendMessage(id, text);
 }
 //InfoTransfer
-function infoTransfer (msg) {
+function infoTransfer(msg) {
   let id = msg.from.id;
-  let text = '📲 Datos de transferencia \n'
-    + ' Entidad👉 Fake Store\n'
-    + ' Banco👉 FakeStoriApi\n'
-    + ' N° de cuenta👉 0000000000';
- return bot.sendMessage(id,text);
+  let text =
+    "📲 Datos de transferencia \n" +
+    " Entidad👉 Fake Store\n" +
+    " Banco👉 FakeStoriApi\n" +
+    " N° de cuenta👉 0000000000";
+  return bot.sendMessage(id, text);
 }
 
-module.exports = { searchProduct, getProducts, postUser, addToCart, getCart, showMainMenu, textDelivery,
-buttonsPayment, infoCash, infoCrypto, infoTransfer };
+module.exports = {
+  searchProduct,
+  getProducts,
+  postUser,
+  addToCart,
+  getCart,
+  showMainMenu,
+  textDelivery,
+  buttonsPayment,
+  infoCash,
+  infoCrypto,
+  infoTransfer,
+};
